725aa6dd015241e964176a96da9111670da807dd
Yurie Shiga <yu.aimer.ri0803@gmail.com>
2019-05-23 13:45:42 -0400

    added review model

--- database/migrations/2019_05_23_145747_create_review_table.php
+++ database/migrations/2019_05_23_145747_create_review_table.php
@@ -15,12 +15,16 @@ class CreateReviewTable extends Migration
     {
         Schema::create('review', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('user_id')->unsigned();
-            $table->integer('business_id')->unsigned();
-            $table->foreign('business_id')->references('id')->on('business');
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('business_id')->unsigned();
             $table->integer('rating');
             $table->text('comment');
             $table->timestamps();
+        });
+
+        Schema::table('review', function ($table) {
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('business_id')->references('id')->on('business');
         });
     }
 
