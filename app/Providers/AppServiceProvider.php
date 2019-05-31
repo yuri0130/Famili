5ee8ef442a0c4d5deb9530657f7177dd2f330a76
Yurie Shiga <yu.aimer.ri0803@gmail.com>
2019-05-31 17:05:29 -0400

    changed host on database.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,7 +23,5 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
-        //
-    }
+    { }
 }
