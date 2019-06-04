00165acbcab8386b0179ba6615c5ed2c64f234d1
Yurie Shiga <yu.aimer.ri0803@gmail.com>
2019-06-04 16:56:54 -0400

    add businesses on BaseTableSeeder

--- database/seeds/BaseTableSeeder.php
+++ database/seeds/BaseTableSeeder.php
@@ -18,34 +18,50 @@ class BaseTableSeeder extends Seeder
         //     'password' => 'password',
         // ]);
 
-        DB::table('business')->insert([
-            'name' => 'Ramon\'s business',
-            'prefecture' => 'Mie',
-            'address' => 'Kuwana-Shi',
-            'contact' => '1234-5678-90',
-            'url' => 'rb.com',
-            'description' => 'Fashionable shoes.',
-            'rating' => 2
+        DB::table('users')->insert([
+            'name' => Str::random(5),
+            'email' => Str::random(5) . '@gmail.com',
+            'password' => bcrypt('secret'),
         ]);
 
+
         DB::table('business')->insert([
-            'name' => 'Minami Koen Park',
-            'prefecture' => 'Tokyo',
-            'address' => 'Kuwana-Shi',
-            'contact' => '1234-5678-90',
-            'url' => 'rb.com',
-            'description' => 'Fashionable shoes.',
+            'name' => '愛知こどもの国',
+            'prefecture' => '愛知県',
+            'address' => '西尾市東幡豆町南越田3番地',
+            'contact' => '0563-62-4151',
+            'url' => 'https://www.kodomo-aichi.jp/',
+            'description' => '広大な児童総合遊園施設です',
             'rating' => 3
         ]);
 
         DB::table('business')->insert([
-            'name' => 'Ramon\'s club',
-            'prefecture' => 'Mie',
-            'address' => 'Kuwana-Shi',
-            'contact' => '1234-5678-90',
-            'url' => 'rb.com',
-            'description' => 'Fashionable shoes.',
+            'name' =>  'Roast&Grill Carne「カルネ」',
+            'prefecture' => '愛知県',
+            'address' => '西尾市永吉１丁目３８',
+            'contact' => '0563-77-4463',
+            'url' =>  ' https://carne.crayonsite.net/ ',
+            'description' => '二階にプレイルームがあり、子連れランチに最適なレストランです',
             'rating' => 4
+        ]);
+
+        DB::table('business')->insert([
+            'name'  => '愛知牧場',
+            'prefecture' => '愛知県',
+            'address' => '日進市米野木町南山９７７',
+            'contact' => '0561-72-1300',
+            'url' => 'http://www.aiboku.com/',
+            'description' => '名古屋市から日帰りで行けるレジャー施設です',
+            'rating' => 4
+        ]);
+        DB::table('business')->insert([
+            'name' =>  '東京ディズニーランド',
+            'prefecture' => '千葉県',
+            'address' => '浦安市舞浜１−１',
+            'contact' => '0570-00-8632',
+            'url' =>  ' https://www.tokyodisneyresort.jp/tdl/',
+            'description' => '千葉県にあるディズニーランドです。東京から電車で行くことができ、子連れにも最適な夢の国です。',
+            'rating' => 5
         ]);
     }
 }
